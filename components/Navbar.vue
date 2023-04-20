745fab94db53ed74f05c5547aba48470246b4cac
Dmytro <dimores95@gmail.com>
2023-04-20 16:55:24 +0300

    project presets

---
+++ components/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+   <header>
+      <div class="container">
+         <div class="logo"></div>
+
+         <div class="search">
+            <input type="text" id="search" />
+         </div>
+
+         <div class="date">
+            <p></p>
+            <time></time>
+         </div>
+      </div>
+   </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ layouts/sidebar.vue
@@ -0,0 +1,13 @@
+<template>
+   <aside>
+      <nav>
+         <ul>
+            <li><a href="#">coming</a></li>
+            <li><a href="#">groups</a></li>
+            <li><a href="#">coming</a></li>
+            <li><a href="#">users</a></li>
+            <li><a href="#">settings</a></li>
+         </ul>
+      </nav>
+   </aside>
+</template>
